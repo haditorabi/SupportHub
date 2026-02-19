a55f56ebf45e7fd7fb4bf04e761a6ae66b621328
Hadi Torabi <83789371+haditorabi@users.noreply.github.com>
2026-02-19 18:56:33 -0500

    Register Services in Program.cs

--- SupportHub.Api/Program.cs
+++ SupportHub.Api/Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using SupportHub.Infrastructure.Persistence;
+using SupportHub.Application.Interfaces;
+using SupportHub.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IAppDbContext>(sp =>
+    sp.GetRequiredService<AppDbContext>());
+
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IAgentService, AgentService>();
+builder.Services.AddScoped<ITicketService, TicketService>();
+builder.Services.AddScoped<ICommentService, CommentService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
